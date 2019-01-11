27b325ce5bb583cdbd6c5ba5c050fe478cbf1702
liyu <liyu@okay.cn>
2019-01-11 14:38:17 +0800

    Update app/Models/Img.php
    图片上传

--- app/Models/Img.php
+++ app/Models/Img.php
@@ -19,7 +19,23 @@ class Img extends Model
     //应该被调整为日期的属性
     protected $dates = ['deleted_at', 'created_at', 'updated_at'];
 
+    /**
+     * @Desc 注入广告位查询
+     * @param $query
+     * @param $position_ids
+     * @return mixed
+     */
+    public function scopeOfPosition ($query, $position_ids)
+    {
+        if (intval($position_ids) > 0) {
+            return $query->where('position_ids', $position_ids);
+        }
+        return $query;
+    }
 
+    /**
+     * @return \Illuminate\Database\Eloquent\Relations\BelongsTo
+     */
     public function positions ()
     {
         return $this->belongsTo(ImagePosition::class, 'position_ids');
